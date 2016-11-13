aaf6a522d8ca8771965062e4d034fdf9f5a302c8
sravani-m16 <sravani.m16@gmail.com>
2016-11-13 11:52:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,11 +93,11 @@ function createTemplate (data) {
 
 
 app.get('/', function (req, res) {
-    var articleName = req.params.articleName;
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName' , function(req, res) {
+   var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
 });
 
