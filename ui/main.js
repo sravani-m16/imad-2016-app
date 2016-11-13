9122b2f276132be51c21d307866f371332fe9894
sravani-m16 <sravani.m16@gmail.com>
2016-11-13 13:19:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById("main-text");
 element.innerHTML = "sravani";
 
 var img = document.getElementById('image');
+var marginLeft = 10;
+function moveRight() {
+    marginLeft = marginLeft+ 10;
+    img.style.marginLeft = marginLeft + 'px'; 
+}
+
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
 };
