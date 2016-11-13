6a96d83a290f70a218a430856713ee4c99273ace
sravani-m16 <sravani.m16@gmail.com>
2016-11-13 13:15:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "sravani";
 
-var img = document.getElementById('img');
+var img = document.getElementById('image');
 img.onclick = function() {
     img.style.marginLeft = '100px' ;
 };
