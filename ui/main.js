db42f86dbfa772ab87f911765c1f0af47d2b68f8
sravani-m16 <sravani.m16@gmail.com>
2016-11-13 19:19:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,17 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
 
-var element = document.getElementById("main-text");
-element.innerHTML = "sravani";
 
-var img = document.getElementById('image');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft+ 1;
-    img.style.marginLeft = marginLeft + 'px'; 
-}
-
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+button.onclick = function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          if(request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  request.open('GET' , 'http://sravani-m16.imad.hasura-app.io/counter' , true);
+  request.send(null);
 };
