2ab724c6d1ba2d3dd3044ad787cc63d44f44f062
sravani-m16 <sravani.m16@gmail.com>
2016-11-20 00:31:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ app.get('/hash/:input', function(req, res) {
 });
 
 app.post('/create-user', function(req, res) {
+    //{"username": "sravani", "password": "password"}
    var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
