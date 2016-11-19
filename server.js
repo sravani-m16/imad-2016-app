5d85795d909974439204c21b880d1d70775a560a
sravani-m16 <sravani.m16@gmail.com>
2016-11-19 20:07:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,6 +56,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+fuction hash(input) {
+    
+}
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input);
+   res.send(hashedString);
+});
+
+
+
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
     pool.query('SELECT * FROM test', function (err, result) {
