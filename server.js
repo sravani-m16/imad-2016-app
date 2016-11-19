1e1c414b22dac6db7c21aa1781219ba25527948a
sravani-m16 <sravani.m16@gmail.com>
2016-11-19 15:09:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function createTemplate (data) {
                    ${heading}
                  </h3>
                  <div>
-                   ${date}
+                   ${date.toDateString()}
                  </div>
                  <div>
                    ${content}
@@ -129,9 +129,19 @@ app.get('/submit-name', function(req, res) {
 });
 
 
-app.get('/:articleName' , function(req, res) {
-   var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName])); 
+app.get('/articles/:articleName' , function(req, res) {
+   pool.query("SELECT * FROM test WHERE title = '" + req.params.articleName + "'", function (err, result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          if(result.rows.length === 0) {
+              res.status(404).send('Article not found');
+          } else {
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
