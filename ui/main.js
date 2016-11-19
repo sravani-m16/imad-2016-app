2e0d5829523aac508aeb567f2408c74368be1066
sravani-m16 <sravani.m16@gmail.com>
2016-11-20 01:59:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,3 @@
-var button = document.getElementById('counter');
-
-
-button.onclick = function() {
-  var request = new XMLHttpRequest();
-  
-  request.onreadystatechange = function() {
-      if(request.readyState == XMLHttpRequest.DONE) {
-          if(request.status == 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-  };
-  request.open('GET' , 'http://sravani-m16.imad.hasura-app.io/counter' , true);
-  request.send(null);
-};
-
-
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
    var request = new XMLHttpRequest();
@@ -25,21 +5,21 @@ submit.onclick = function() {
   request.onreadystatechange = function() {
       if(request.readyState == XMLHttpRequest.DONE) {
           if(request.status == 200) {
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i<names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
+             alert('Logged in successfully');
+          } else if(request.status == 403) {
+              alert('username/password is incorrect');
+          } else if(request.status == 500) {
+              alert('Something went wrong on the server');
           }
       }
   };
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET' , 'http://sravani-m16.imad.hasura-app.io/submit-name?name=' + name , true);
-  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST' , 'http://sravani-m16.imad.hasura-app.io/submit-name?name=' + name , true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password:password}));
 };
   
  
