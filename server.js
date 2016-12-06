e8293975c0f0a55168cb50d5cf3dcb2b1d4489dc
sravani-m16 <sravani.m16@gmail.com>
2016-12-06 14:09:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,9 +25,12 @@ app.use(session({
 
 function createTemplate (data) {
     var title = data.title;
+    var subtitle = data.subtitle;
     var date = data.date;
     var heading = data.heading;
+    var authir = data.author;
     var content = data.content;
+    var backgroundimage = data.backgroundimage;
     
     var htmlTemplate = `
     <!DOCTYPE html>
@@ -41,7 +44,7 @@ function createTemplate (data) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Creative - Start Bootstrap Theme</title>
+    <title>${title}</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -82,16 +85,16 @@ function createTemplate (data) {
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a class="page-scroll" href="#about">About</a>
+                        <a class="page-scroll" href="index.html">Home</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#services">Services</a>
+                        <a class="page-scroll" href="about.html">About</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#portfolio">Portfolio</a>
+                        <a class="page-scroll" href="article.html">Articles</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
+                        <a class="page-scroll" href="contact.html">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -103,10 +106,9 @@ function createTemplate (data) {
     <header>
         <div class="header-content">
             <div class="header-content-inner">
-                <h1 id="homeHeading">Your Favorite Source of Free Bootstrap Themes</h1>
-                <hr>
-                <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
-                <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+                <h1 class="homeHeading">${heading}</h1>
+                <h2 class="subheading">${subtitle}</h2>
+                <span class="meta">Posted by  <a href="#">${author}</a> on ${date.toDateString()}</span>
             </div>
         </div>
     </header>
@@ -115,181 +117,58 @@ function createTemplate (data) {
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">We've got what you need!</h2>
-                    <hr class="light">
-                    <p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-                    <a href="#services" class="page-scroll btn btn-default btn-xl sr-button">Get Started!</a>
+                    <a href="about.html">${content}</a>
                 </div>
             </div>
         </div>
     </section>
 
-    <section id="services">
+    <section id="articles">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">At Your Service</h2>
+                    <h2 class="section-heading">Articles</h2>
                     <hr class="primary">
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
-                        <h3>Sturdy Templates</h3>
-                        <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
-                        <h3>Ready to Ship</h3>
-                        <p class="text-muted">You can use this theme as is, or you can make changes!</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
-                        <h3>Up to Date</h3>
-                        <p class="text-muted">We update dependencies to keep things fresh.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
-                        <h3>Made with Love</h3>
-                        <p class="text-muted">You have to make your websites with love these days!</p>
-                    </div>
-                </div>
+                <a href="articles.html">
+                ${content}
+                </a>
             </div>
         </div>
     </section>
-
-    <section class="no-padding" id="portfolio">
-        <div class="container-fluid">
-            <div class="row no-gutter popup-gallery">
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/1.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/1.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/2.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/3.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/3.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/4.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/4.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/5.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/5.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="img/portfolio/fullsize/6.jpg" class="portfolio-box">
-                        <img src="img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <aside class="bg-dark">
-        <div class="container text-center">
-            <div class="call-to-action">
-                <h2>Free Download at Start Bootstrap!</h2>
-                <a href="http://startbootstrap.com/template-overviews/creative/" class="btn btn-default btn-xl sr-button">Download Now!</a>
-            </div>
-        </div>
-    </aside>
 
     <section id="contact">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">Let's Get In Touch!</h2>
-                    <hr class="primary">
-                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
-                </div>
-                <div class="col-lg-4 col-lg-offset-2 text-center">
-                    <i class="fa fa-phone fa-3x sr-contact"></i>
-                    <p>123-456-6789</p>
-                </div>
-                <div class="col-lg-4 text-center">
-                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
-                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
-                </div>
-            </div>
+            <ul class="list-inline text-center">
+                        <li>
+                            <a href="https://twitter.com/aniket_aditya">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/aniket.aditya">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/aniketaditya">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
         </div>
     </section>
 
@@ -459,6 +338,14 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
+
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+   res.send(counter.toString()); 
+});
+
+
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function (err, result) {
@@ -479,6 +366,112 @@ app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/article.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article.js'));
+});
+
+app.get('/index.html', function(req, res){
+   res.sendFile(path.join(__dirname, 'ui','index.html')); 
+});
+
+app.get('/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+});
+
+app.get('/contact.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+});
+
+app.get('/css/my-blog.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/css', 'my-blog.css'));
+});
+
+app.get('/js/contact_me.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/js', 'contact_me.js'));
+});
+
+app.get('/mail/contact_me.php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/mail', 'contact_me.php'));
+});
+
+app.get('/js/jqBootstrapValidation.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/js', 'jqBootstrapValidation.js'));
+});
+
+app.get('/js/my-blog.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/js', 'my-blog.js'));
+});
+
+app.get('/vendor/bootstrap/css/bootstrap.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/css', 'bootstrap.css'));
+});
+
+app.get('/vendor/bootstrap/js/bootstrap.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/js', 'bootstrap.js'));
+});
+
+app.get('/vendor/font-awesome/css/font-awesome.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/font-awesome/css', 'font-awesome.css'));
+});
+
+app.get('/vendor/jquery/jquery.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/jquery', 'jquery.js'));
+});
+
+app.get('/img/about-bg.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'about-bg.jpg'));
+});
+
+app.get('/img/contact.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'contact.jpg'));
+});
+
+app.get('/img/home-bg.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'home-bg.jpg'));
+});
+
+app.get('/img/image.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'image.jpg'));
+});
+
+app.get('/img/technology.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'technology.jpg'));
+});
+
+app.get('/img/contact_me.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'contact_me.jpg'));
+});
+
+app.get('/img/javascript1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'javascript1.jpg'));
+});
+
+app.get('/img/web_security.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/img', 'web_security'));
+});
+
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.ttf', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts/', 'glyphicons-halflings.ttf'));
+});
+
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.svg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts/', 'glyphicons-halflings.svg'));
+});
+
+app.get('/vendor/font-awesome/fonts/fontawesome-webfont.ttf', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/font-awesome/fonts' , 'fontawesome-webfont.ttf'));
+});
+
+app.get('/vendor/font-awesome/fonts/fontawesome-webfont.woff', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/font-awesome/fonts' , 'fontawesome-webfont.woff'));
+});
+  app.get('/vendor/font-awesome/fonts/fontawesome-webfont.woff2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/font-awesome/fonts' , 'fontawesome-webfont.woff2'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
